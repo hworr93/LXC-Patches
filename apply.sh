6a4e8e18b210bdb356803b870143243ea27544d7
hworr93 <hworr93@gmail.com>
2026-04-02 06:57:26 -0500

    Update and rename apply.sh to config-kernel.sh

--- apply.sh
+++
@@ -1,7 +0,0 @@
-# Loop through and apply all .patch files found in the repo
-for patch in ../LXC-Patches/*.patch; do
-  if [ -f "$patch" ]; then
-    echo -e "\e[32mApplying:\e[0m $patch"
-    patch -p1 -F 3 < "$patch"
-  fi
-done

bb2a916e61ca9e0e8b8a211ea705f4b7441968e8
hworr93 <hworr93@gmail.com>
2026-04-02 06:52:10 -0500

    Refactor patch application loop for readability

--- apply.sh
+++ apply.sh
@@ -1,7 +1,7 @@
 # Loop through and apply all .patch files found in the repo
-         for patch in ../LXC-Patches/*.patch; do
-           if [ -f "$patch" ]; then
-             echo -e "\e[32mApplying:\e[0m $patch"
-             patch -p1 -F 3 < "$patch"
-           fi
-         done
+for patch in ../LXC-Patches/*.patch; do
+  if [ -f "$patch" ]; then
+    echo -e "\e[32mApplying:\e[0m $patch"
+    patch -p1 -F 3 < "$patch"
+  fi
+done
